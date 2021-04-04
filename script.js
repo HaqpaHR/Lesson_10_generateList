0dab3d68d41ee32d664ce0246115f93b15779403
HaqpaHR <pereverzev.viacheslav@gmail.com>
2021-04-04 16:19:01 +0300

    changed1

--- script.js
+++ script.js
@@ -2,24 +2,18 @@ function generateList(arr){
   let ul = document.createElement("ul");
   
   arr.forEach((element)=>{
-     const li = document.createElement("li");
-     let ch;
-
-     if(Array.isArray(element)){
-      ch = generateList(element);
-     }else{
-      ch = document.createTextNode(element)
-     };
-
-     li.appendChild(ch);
-     ul.appendChild(li);
+    if (Array.isArray(element)) {
+        ul.appendChild(generateList(element));
+        } else {
+        const li = document.createElement("li");
+        li.innerText = element;
+        ul.appendChild(li);
+      }
   })
   return ul
 }
    
-  const array = [1, 2, [1.1, 1.2, [5.1, 5.2],
-    [6.1, 6.2], 1.3
-  ], 3];
+  const array = [1, 2, [1.1, 1.2, [5.1, 5.2],[6.1, 6.2], 1.3], 3];
    
   const s = generateList(array);
   document.body.appendChild(s)
